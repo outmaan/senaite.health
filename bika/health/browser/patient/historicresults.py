5ddcf29712f00c71d44c1b5efa654816bb1eba3c
Jordi Puiggené <jpuiggene@naralabs.com>
2018-06-20 12:43:57 +0200

    Improve (performance) Patient's historic results view

--- bika/health/browser/patient/historicresults.py
+++ bika/health/browser/patient/historicresults.py
@@ -7,8 +7,12 @@
 
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from bika.lims import api
 from bika.lims.browser import BrowserView
 from bika.health import bikaMessageFactory as _
+from bika.lims.catalog.analysisrequest_catalog import \
+    CATALOG_ANALYSIS_REQUEST_LISTING
+from bika.lims.catalog.analysis_catalog import CATALOG_ANALYSIS_LISTING
 from zope.interface import implements
 from plone.app.layout.globals.interfaces import IViewView
 from Products.CMFPlone.i18nl10n import ulocalized_time
@@ -75,53 +79,51 @@ def get_historicresults(patient):
 
     rows = {}
     dates = []
-    uid = patient.UID()
     states = ['verified', 'published']
 
     # Retrieve the AR IDs for the current patient
-    bc = getToolByName(patient, 'bika_catalog')
-    ars = [ar.id for ar
-           in bc(portal_type='AnalysisRequest', review_state=states)
-           if 'Patient' in ar.getObject().Schema()
-           and ar.getObject().Schema().getField('Patient').get(ar.getObject())
-           and ar.getObject().Schema().getField('Patient').get(ar.getObject()).UID() == uid]
+    query = dict(getPatientUID=api.get_uid(patient))
+    ars = api.search(query, catalog=CATALOG_ANALYSIS_REQUEST_LISTING)
+    ar_uids = map(api.get_uid, ars)
 
     # Retrieve all the analyses, sorted by ResultCaptureDate DESC
-    bc = getToolByName(patient, 'bika_analysis_catalog')
-    analyses = [an.getObject() for an
-                in bc(portal_type='Analysis',
-                      getRequestID=ars,
-                      sort_on='getResultCaptureDate',
-                      sort_order='reverse')]
+    query = dict(getRequestUID=ar_uids, sort_on='getResultCaptureDate',
+                 sort_order='reverse')
+    analyses = api.search(query, catalog=CATALOG_ANALYSIS_LISTING)
 
     # Build the dictionary of rows
     for analysis in analyses:
-        ar = analysis.aq_parent
-        sampletype = ar.getSampleType()
-        row = rows.get(sampletype.UID()) if sampletype.UID() in rows.keys() \
-            else {'object': sampletype, 'analyses': {}}
+        analysis = api.get_object(analysis)
+        ar = analysis.getRequest()
+        sample_type = ar.getSampleType()
+        if not sample_type:
+            continue
+        sample_type_uid = api.get_uid(sample_type)
+        row = rows.get(sample_type_uid, dict(object=sample_type, analyses={}))
         anrow = row.get('analyses')
         service_uid = analysis.getServiceUID()
         asdict = anrow.get(service_uid, {'object': analysis,
                                          'title': analysis.Title(),
                                          'keyword': analysis.getKeyword(),
                                          'units': analysis.getUnit()})
-        date = analysis.getResultCaptureDate() or analysis.created()
-        date = ulocalized_time(date, 1, None, patient, 'bika')
+        cap_date = analysis.getResultCaptureDate()
+        cap_date = api.is_date(cap_date) and \
+                   cap_date.strftime('%Y-%m-%d %I:%M %p') or ''
+        if not cap_date:
+            return
+
         # If more than one analysis of the same type has been
         # performed in the same datetime, get only the last one
-        if date not in asdict.keys():
-            asdict[date] = {'object': analysis,
-                            'result': analysis.getResult(),
-                            'formattedresult': analysis.getFormattedResult()}
+        if cap_date not in asdict.keys():
+            asdict[cap_date] = {'object': analysis,
+                                'result': analysis.getResult(),
+                                'formattedresult': analysis.getFormattedResult()}
             # Get the specs
             # Only the specs applied to the last analysis for that
             # sample type will be taken into consideration.
             # We assume specs from previous analyses are obsolete.
             if 'specs' not in asdict.keys():
-                spec = analysis.getAnalysisSpecs()                
-                spec = spec.getResultsRangeDict() if spec else {}
-                specs = spec.get(analysis.getKeyword(), {})
+                specs = analysis.getResultsRange()
                 if not specs.get('rangecomment', ''):
                     if specs.get('min', '') and specs.get('max', ''):
                         specs['rangecomment'] = '%s - %s' % \
@@ -138,11 +140,11 @@ def get_historicresults(patient):
                                                   specs.get('error'))) + '%)'
                 asdict['specs'] = specs
 
-            if date not in dates:
-                dates.append(date)
+            if cap_date not in dates:
+                dates.append(cap_date)
         anrow[service_uid] = asdict
         row['analyses'] = anrow
-        rows[sampletype.UID()] = row
+        rows[sample_type_uid] = row
     dates.sort(reverse=False)
 
     return dates, rows
